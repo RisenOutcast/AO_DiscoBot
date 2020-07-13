89c1d7b0483d7e9c00807b1bcb619a144d6f9f20
Metso <the.arctic.dragon.knight@gmail.com>
2020-07-13 19:15:56 +0300

    Update search.py

--- cogs/search.py
+++ cogs/search.py
@@ -66,13 +66,14 @@ class search(commands.Cog):
             embed.add_field(name='Alliance', value='Not in a alliance', inline=True)
 
 
-        embed.add_field(name='Kill Fame', value=data['players'][0]['KillFame'], inline=False)
-        embed.add_field(name='Death Fame', value=data['players'][0]['DeathFame'], inline=False)
-        embed.add_field(name='Fame Ratio', value=data['players'][0]['FameRatio'], inline=False)
-        embed.add_field(name='Total Kills', value=data['players'][0]['totalKills'], inline=False)
-        embed.add_field(name='GvG Kills', value=data['players'][0]['gvgKills'], inline=False)
-        embed.add_field(name='GvG Wins', value=data['players'][0]['gvgWon'], inline=False)
-        embed.add_field(name='test',value='test \n to \n see \n if \n this \n fucking \n works', inline=False)
+        #embed.add_field(name='Kill Fame', value=data['players'][0]['KillFame'], inline=False)
+        #embed.add_field(name='Death Fame', value=data['players'][0]['DeathFame'], inline=False)
+        #embed.add_field(name='Fame Ratio', value=data['players'][0]['FameRatio'], inline=False)
+        #embed.add_field(name='Total Kills', value=data['players'][0]['totalKills'], inline=False)
+        #embed.add_field(name='GvG Kills', value=data['players'][0]['gvgKills'], inline=False)
+        #embed.add_field(name='GvG Wins', value=data['players'][0]['gvgWon'], inline=False)
+        embed.add_field(name='Stats',value='**Kill Fame**: ' + str(self.comma_me(data['players'][0]['KillFame'])) + '\n' + '**Death Fame**: ' + str(self.comma_me(data['players'][0]['DeathFame'])) + '\n' + '**Fame Ratio**: ' + str(data['players'][0]['FameRatio']) + '\n ' + '**Total Kills**: ' + str(self.comma_me(data['players'][0]['totalKills'])) + '\n ' + '**GvG Kills**: ' + str(self.comma_me(data['players'][0]['gvgKills'])) + '\n ' + '**GvG Wins**: ' + str(self.comma_me(data['players'][0]['gvgWon'])), inline=False)
+        #embed.add_field(name='Value',value=str(data['players'][0]['KillFame']) + '\n' + str(data['players'][0]['DeathFame']) + '\n' + str(data['players'][0]['FameRatio']) + '\n' + str(data['players'][0]['totalKills']) + '\n' + str(data['players'][0]['gvgKills']) + '\n' + str(data['players'][0]['gvgWon']), inline=True)
         print(data['players'][0]['KillFame'])
         print(data['players'][0]['DeathFame'])
         print(data['players'][0]['FameRatio'])
@@ -84,15 +85,26 @@ class search(commands.Cog):
         b = requests.get(url = URL2)
         data2 = b.json()
 
-        embed.add_field(name='PvE Fame', value=data2['LifetimeStatistics']['PvE']['Total'], inline=False)
-        embed.add_field(name='Gathering Fame', value=data2['LifetimeStatistics']['Gathering']['All']['Total'], inline=False)
-        embed.add_field(name='Crafting Fame', value=data2['LifetimeStatistics']['Crafting']['Total'], inline=False)
-        print(data2['LifetimeStatistics']['PvE']['Total'])
-        print(data2['LifetimeStatistics']['Gathering']['All']['Total'])
-        print(data2['LifetimeStatistics']['Crafting']['Total'])
-        print(data2['LifetimeStatistics']['CrystalLeague'])
+        embed.add_field(name='PvE Fame', value=self.comma_me(data2['LifetimeStatistics']['PvE']['Total']), inline=False)
+        embed.add_field(name='Gathering Fame', value=self.comma_me(data2['LifetimeStatistics']['Gathering']['All']['Total']), inline=False)
+        embed.add_field(name='Crafting Fame', value=self.comma_me(data2['LifetimeStatistics']['Crafting']['Total']), inline=False)
+        print(self.comma_me(data2['LifetimeStatistics']['PvE']['Total']))
+        print(self.comma_me(data2['LifetimeStatistics']['Gathering']['All']['Total']))
+        print(self.comma_me(data2['LifetimeStatistics']['Crafting']['Total']))
+        print(self.comma_me(data2['LifetimeStatistics']['CrystalLeague']))
 
         await ctx.send(embed=embed)
+
+    #Make numbers more human-friendly
+    def comma_me(self, number): 
+        if number is None:
+            return
+        else:
+            if number is '':
+                return
+            else:
+                return ("{:,}".format(number)) 
+  
     
 
 def setup(client):
